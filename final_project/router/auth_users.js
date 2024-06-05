0bb56e70dda056f8109ca1081f231698f196c893
wangxi <wangxi009567@sina.com>
2024-06-05 14:38:11 +0800

    Finished task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -8,6 +8,7 @@ let users = [];
 const isValid = (username) => {
   //returns boolean
   //write code to check is the username is valid
+  return users.filter((user) => user.username === username).length > 0;
 };
 
 const authenticatedUser = (username, password) => {
@@ -57,7 +58,41 @@ regd_users.post("/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (!req.query.review) {
+    return res.status(204).json({ message: "Please enter review information" });
+  }
+  if (!books[req.params.isbn]) {
+    return res.status(404).json({
+      message: "Cannot find the book witch ISBN is " + req.params.isbn,
+    });
+  }
+  books[req.params.isbn].reviews[req.session.authorization.username] =
+    req.query.review;
+  return res.status(200).json({
+    message:
+      "The review for the book with ISBN " +
+      req.params.isbn +
+      " has been added/updated.",
+  });
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  if (!books[req.params.isbn]) {
+    return res.status(404).json({
+      message: "Cannot find the book witch ISBN is " + req.params.isbn,
+    });
+  }
+  delete books[req.params.isbn].reviews[req.session.authorization.username];
+  return res.status(200).json({
+    message:
+      "Reviews for the ISBN " +
+      req.params.isbn +
+      " posted by the user " +
+      req.session.authorization.username +
+      " deleted.",
+  });
 });
 
 module.exports.authenticated = regd_users;
