d1eed2f624e3c1bebca1f912f64812501405c8f0
wangxi <wangxi009567@sina.com>
2024-06-05 13:37:53 +0800

    Finished task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,13 +67,27 @@ public_users.get("/author/:author", function (req, res) {
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (Object.values(books).some((book) => book.title === req.params.title)) {
+    return res
+      .status(200)
+      .json(
+        Object.values(books).find((book) => book.title === req.params.title)
+      );
+  }
+  return res.status(404).json({
+    message: "Cannot find the book witch title is " + req.params.title,
+  });
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  if (books[req.params.isbn]) {
+    return res.status(200).json(books[req.params.isbn].reviews);
+  }
+  return res
+    .status(404)
+    .json({ message: "Cannot find the book witch ISBN is " + req.params.isbn });
 });
 
 module.exports.general = public_users;
